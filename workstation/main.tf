d55b3bccc8d7e3688ae8f230960a361934e56b99
Nell Shamrell <nellshamrell@gmail.com>
2016-03-17 00:27:21 -0400

    revising workstation config

--- workstation/main.tf
+++ workstation/main.tf
@@ -7,16 +7,17 @@ resource "template_file" "knife_rb" {
   }
   # Make .chef/knife.rb file
   provisioner "local-exec" {
-    command = "mkdir -p .chef && echo '${template_file.knife_rb.rendered}' > .chef/knife.rb"
+    command = "mkdir -p ../.chef && echo '${template_file.knife_rb.rendered}' > ../.chef/knife.rb"
   }
 
   # Download chef validation pem
   provisioner "local-exec" {
-    command = "scp -i ${var.private_ssh_key_path} ubuntu@${var.chef-server-fqdn}:${var.chef-server-user}.pem ."
+    command = "scp -oStrictHostKeyChecking=no -i ${var.private_ssh_key_path} ubuntu@${var.chef-server-fqdn}:${var.chef-server-user}.pem ../.chef"
   }
 
   # Fetch Chef Server Certificate
   provisioner "local-exec" {
-    command = "knife ssl fetch"
+    # changing to the parent directory so the trusted cert goes into ../.chef/trusted_certs
+    command = "cd .. && knife ssl fetch"
   }
 }

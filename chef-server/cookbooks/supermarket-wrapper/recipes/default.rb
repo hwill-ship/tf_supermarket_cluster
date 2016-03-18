288ef4c8b8c80828394c5b3eb3c988236f08bab4
Nell Shamrell <nellshamrell@gmail.com>
2016-03-17 20:42:15 -0400

    adds in supermarket wrapper cookbook

---
+++ chef-server/cookbooks/supermarket-wrapper/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: supermarket-wrapper
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+app = data_bag_item('apps', 'supermarket')
+node.set['supermarket_omnibus']['chef_server_url'] = app['chef_server_url']
+node.set['supermarket_omnibus']['chef_oauth2_app_id'] = app['uid']
+node.set['supermarket_omnibus']['chef_oauth2_secret'] = app['secret']
+node.set['supermarket_omnibus']['config']['fqdn'] = app['fqdn']
+node.set['supermarket_omnibus']['config']['host'] = app['fqdn']
+
+include_recipe 'supermarket-omnibus-cookbook'

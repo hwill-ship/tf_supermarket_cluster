d645d966d5cad0e9578596fd77bd8bfd431314a7
Nell Shamrell <nellshamrell@gmail.com>
2016-03-29 11:19:40 -0400

    wip

--- main.tf
+++ main.tf
@@ -49,6 +49,7 @@ module "workstation" {
   private_ssh_key_path = "${var.private_ssh_key_path}"
 }
 
+/*
 module "databags" {
   source = "./databags"
   chef-server-fqdn = "${module.chef-server.public_ip}"
@@ -63,4 +64,108 @@ module "nodes" {
   source = "./nodes"
   supermarket-server-public-ip = "${module.supermarket-server.public_ip}"
   private_ssh_key_path = "${var.private_ssh_key_path}"
+}
+*/
+
+
+# Configure the Chef provider
+# This is commented out because it is current failing with the error "chef_data_bag.apps: Post https://chef-server-fqdn/organizations/data: x509: cannot validate certificate for chef-server-fqdn because it doesn't contain any IP SANs"
+/*
+provider "chef" {
+  server_url = "https://${module.chef-server.public_ip}/organizations/${var.chef-server-org-name}"
+
+ client_name = "${var.chef-server-user}"
+ private_key_pem = "${file(".chef/${var.chef-private-key-path}.pem")}"
+}
+
+resource "chef_data_bag" "apps" {
+    name = "supermarket"
+}
+*/
+
+resource "null_resource" "supermarket-oc-id-info" {
+   # Changes ownership of /etc/opscode/oc-id-applications/supermarket.json on the Chef Server
+  # So it can be pulled down to the local workstation using the ubuntu user
+  # Force sleep for 60 seconds so other modules have a chance to finish
+  provisioner "local-exec" {
+    command = "sleep 60 && ssh -i ${var.private_ssh_key_path} ubuntu@${module.chef-server.public_ip} 'sudo chown ubuntu /etc/opscode/oc-id-applications/supermarket.json'"
+  }
+
+  # Pulls down supermarket oc-id config from the Chef server 
+  provisioner "local-exec" {
+    command = "scp -i ${var.private_ssh_key_path} ubuntu@${module.chef-server.public_ip}:/etc/opscode/oc-id-applications/supermarket.json ."
+  } 
+
+  # Extract uid from supermarket oc-id config 
+  provisioner "local-exec" {
+    command = "grep -Po '\"uid\".*?[^\\\\]\",' supermarket.json > uid.txt"
+  }
+
+  # Extract secret from supermarket oc-id config
+  provisioner "local-exec" {
+    command = "grep -Po '\"secret\".*?[^\\\\]\"(?=,)' supermarket.json > secret.txt"
+  }
+}
+
+resource "null_resource" "supermarket-databag-setup" {
+  # Make a data bags directory
+  provisioner "local-exec" {
+    command = "mkdir -p databags/apps"
+  }
+
+  # Make json file for supermarket data bag item
+  # Using a heredoc, rather than a template
+  # Because I could not pass the values of oc-id.txt and secret.txt
+  # to the template because they are dynamically created when the terraform
+  # config runs
+  provisioner "local-exec" {
+    command = <<EOF
+    cat <<FILE > databags/apps/supermarket.json
+{
+  "id": "supermarket",
+  "fqdn": "${module.supermarket-server.public_ip}",
+  "chef_server_url": "https://${module.chef-server.public_ip}",
+  ${file("uid.txt")}
+  ${file("secret.txt")} 
+}
+FILE
+EOF
+  }
+
+  # Create the apps data bag on the Chef server
+  provisioner "local-exec" {
+  # Sleep 60 is a hack so that this module will not run until the workstation module is complete
+  # Currently terraform will not allow you to use depends_on with a module
+  # https://github.com/hashicorp/terraform/issues/1178
+
+    command = "sleep 120 && knife data bag create apps"
+  }
+
+  # Create supermarket data bag item on the Chef server
+  provisioner "local-exec" {
+    command = "knife data bag from file apps databags/apps/supermarket.json"
+  }  
+}
+
+resource "null_resource" "supermarket-node-setup" {
+  depends_on = ["null_resource.supermarket-databag-setup"]
+  # Forces 360 second wait to allow other modules to finish before this one
+  # Terraform does not currently let you use depends_on with a module
+  provisioner "local-exec" {
+    command = "sleep 360 && knife bootstrap ${module.supermarket-server.public_ip} -i ${var.private_ssh_key_path} -N supermarket-node -x ubuntu --sudo"
+  }
+}
+
+resource "null_resource" "configure-supermarket-node-run-list" {
+  depends_on = ["null_resource.supermarket-node-setup"]
+  provisioner "local-exec" {
+    command = "knife node run_list add supermarket-node 'recipe[supermarket-wrapper::default]'"
+  }
+}
+
+resource "null_resource" "supermarket-node-client" {
+  depends_on = ["null_resource.configure-supermarket-node-run-list"]
+  provisioner "local-exec" {
+    command = "ssh -i ${var.private_ssh_key_path} ubuntu@${module.supermarket-server.public_ip} 'sudo chef-client'"
+  }
 }
